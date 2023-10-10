d81f6b35d5d3ee489eece47844123c332a4a9588
Viktoria <victoria.todorova0@gmail.com>
2023-10-10 22:21:16 +0200

    axios.get request through the API KEY

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const ejs = require('ejs');
-const apiKey = process.env["API_KEY "];
+const apiKey = process.env.API_KEY ;
 
 app.listen(3000);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+
 
 app.set('view engine', 'ejs'); // Use 'ejs' as the view engine
 
@@ -18,3 +21,25 @@ app.get('/home', (req, res) => {
     res.render('home', { title: 'Home' });
 });
 
+
+
+app.get('/weather', (req, res) => {
+    const { lat, lon } = req.query;
+
+    const params = {
+        lat,
+        lon,
+        appid: apiKey,
+    };
+
+    axios
+        .get('https://api.openweathermap.org/data/2.5/weather', { params })
+        .then((response) => {
+            const apiResponse = response.data;
+            res.json(apiResponse); // Send the API response as JSON to the client
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({ error: 'An error occurred' });
+        });
+});
