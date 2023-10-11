90eb27d619548861b75f1fea226046b9a8c483f0
Viktoria <victoria.todorova0@gmail.com>
2023-10-11 15:46:22 +0200

    redirecting to another page with the data through a user's input

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const ejs = require('ejs');
 const apiKey = process.env.API_KEY ;
 
-app.listen(3000);
+app.listen(3999);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
@@ -24,22 +24,52 @@ app.get('/home', (req, res) => {
 
 
 app.get('/weather', (req, res) => {
-    const { lat, lon } = req.query;
+    const city = req.query.city;
 
-    const params = {
-        lat,
-        lon,
-        appid: apiKey,
-    };
+    const apiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
 
     axios
-        .get('https://api.openweathermap.org/data/2.5/weather', { params })
+        .get(apiURL)
         .then((response) => {
-            const apiResponse = response.data;
-            res.json(apiResponse); // Send the API response as JSON to the client
+            const data = response.data;
+            const lat = response.data[0].lat;
+            const lon = response.data[0].lon;
+
+
+                // weatherDescription: "few clouds",
+                // Include other relevant weather data here
+
+
+
+
+
+
+            const apiURLcurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+axios.get(apiURLcurrent)
+                .then((response)=>{
+
+                    const dataCurrent = response.data;
+                    console.log(dataCurrent);
+                    const mainData = dataCurrent.weather[0].main;
+                    const mainDescription = dataCurrent.weather[0].description;
+
+                    const temperature = dataCurrent.main.temp;
+                    const feelsTemp = dataCurrent.main.feels_like;
+                    const maxTemp = dataCurrent.main.temp_max;
+                    const minTemp = dataCurrent.main.temp_min;
+                    const wind = dataCurrent.wind.speed;
+
+
+
+                    res.render('weather', {title: 'Weather',mainData:mainData,mainDescription:mainDescription,temperature:temperature,feelsTemp:feelsTemp,maxTemp:maxTemp,minTemp:minTemp,wind:wind});
+                })
+    .catch((error) => {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred' });
+    });
+
         })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).json({ error: 'An error occurred' });
-        });
+
+
 });
