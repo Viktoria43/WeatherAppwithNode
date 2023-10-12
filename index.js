fd9f334a4a06114696656d30a175be5284dc7083
Viktoria <victoria.todorova0@gmail.com>
2023-10-12 16:25:20 +0200

    code reformat

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const ejs = require('ejs');
-const apiKey = process.env.API_KEY ;
+const apiKey = process.env.API_KEY;
 app.listen(3999);
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs'); // Use 'ejs' as the view engine
 
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/home', (req, res) => {
-    res.render('home', { title: 'Home' });
+    res.render('home', {title: 'Home'});
 });
 
 app.get('/weather', (req, res) => {
@@ -29,13 +29,13 @@ app.get('/weather', (req, res) => {
             const lon = response.data[0].lon;
             const apiURLcurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
-axios
-    .get(apiURLcurrent)
-                .then((response)=>{
+            axios
+                .get(apiURLcurrent)
+                .then((response) => {
 
                     const dataCurrent = response.data;
                     const mainData = dataCurrent.weather[0].main;
-                    let backgroundClass =    'backgroundClass';
+                    let backgroundClass = 'backgroundClass';
                     if (mainData != 'Clear') {
                         backgroundClass = 'clouds';
                     }
@@ -48,13 +48,23 @@ axios
                     const wind = dataCurrent.wind.speed;
 
 
-
-                    res.render('weather', {title: 'Weather',mainData:mainData,mainDescription:mainDescription,temperature:temperature,feelsTemp:feelsTemp,maxTemp:maxTemp,minTemp:minTemp,wind:wind,city:city,backgroundClass:backgroundClass});
+                    res.render('weather', {
+                        title: 'Weather',
+                        mainData: mainData,
+                        mainDescription: mainDescription,
+                        temperature: temperature,
+                        feelsTemp: feelsTemp,
+                        maxTemp: maxTemp,
+                        minTemp: minTemp,
+                        wind: wind,
+                        city: city,
+                        backgroundClass: backgroundClass
+                    });
                 })
-    .catch((error) => {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred' });
-    });
+                .catch((error) => {
+                    console.error(error);
+                    res.status(500).json({error: 'An error occurred'});
+                });
 
         })
 });
